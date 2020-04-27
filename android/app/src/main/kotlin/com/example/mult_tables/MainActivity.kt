d69ee7605b347beafb5fdf6ef91115d1063dd7c4
bit00 <akbrijesh@gmail.com>
2020-04-27 21:39:20 +0530

    initial empty app

---
+++ android/app/src/main/kotlin/com/example/mult_tables/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mult_tables
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Multi Tables',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Multi Tables'),
+        ),
+        body: Center(
+          child: Text('Multi Tables'),
+        ),
+      ),
+    );
+  }
+}
