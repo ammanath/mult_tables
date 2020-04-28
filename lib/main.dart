a88d745c38bb690358b6583a338d2dd8dfdb3703
bit00 <akbrijesh@gmail.com>
2020-04-28 10:58:52 +0530

    added gridview

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Multi Tables',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.lime,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       debugShowCheckedModeBanner: false,
@@ -19,9 +19,48 @@ class MyApp extends StatelessWidget {
           title: Text('Multi Tables'),
         ),
         body: Center(
-          child: Text('Multi Tables'),
+          child: MainBodyWidget(),
         ),
       ),
     );
   }
+}
+
+class MainBodyWidget extends StatelessWidget {
+  const MainBodyWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return  Container(
+      color: Colors.lime,
+      child: GridWidget(),
+    );
+
+  }
+  
+}
+
+class GridWidget extends StatelessWidget {
+  const GridWidget({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.extent(
+    maxCrossAxisExtent: 120,
+    padding: const EdgeInsets.all(4),
+    mainAxisSpacing: 4,
+    crossAxisSpacing: 4,
+    children: _buildGridTileList(99));
+  }
+
+  List<Container> _buildGridTileList(int count) => List.generate(
+    count, (i) => Container(
+      color: Colors.lime[200],
+      child: Center(child: Text((i+2).toString()))
+      ));
 }
