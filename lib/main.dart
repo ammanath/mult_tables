e437b0e97833b5d7a562f1dbe1476374cc7509cd
bit00 <akbrijesh@gmail.com>
2020-06-13 15:00:04 +0530

    minor change

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.lime,
       ),
       debugShowCheckedModeBanner: false,
-      home: SafeArea(child: QuizPageWidget()),
+      home: SafeArea(child: HomePageWidget()),
     );
   }
 }
